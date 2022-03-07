0b58fdbbf585f33011d54392b87281f115662c96
sb | open-admin.org <info@open-admin.org>
2022-03-07 10:52:59 +0100

    init

---
+++ src/BootExtension.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace OpenAdmin\Admin\LogViewer;
+
+use OpenAdmin\Admin\Admin;
+
+trait BootExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function boot()
+    {
+        static::registerRoutes();
+
+        Admin::extend('log-viewer', __CLASS__);
+    }
+
+    /**
+     * Register routes for laravel-admin.
+     *
+     * @return void
+     */
+    protected static function registerRoutes()
+    {
+        parent::routes(function ($router) {
+            /* @var \Illuminate\Routing\Router $router */
+            $router->get('logs', 'OpenAdmin\Admin\LogViewer\LogController@index')->name('log-viewer-index');
+            $router->get('logs/{file}', 'OpenAdmin\Admin\LogViewer\LogController@index')->name('log-viewer-file');
+            $router->get('logs/{file}/tail', 'OpenAdmin\Admin\LogViewer\LogController@tail')->name('log-viewer-tail');
+        });
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function import()
+    {
+        parent::createMenu('Log viewer', 'logs', 'fa-database');
+
+        parent::createPermission('Logs', 'ext.log-viewer', 'logs*');
+    }
+}
---
+++ src/LogViewerServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OpenAdmin\Admin\LogViewer;
+
+use Illuminate\Support\ServiceProvider;
+
+class LogViewerServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-admin-logs');
+
+        LogViewer::boot();
+    }
+}
